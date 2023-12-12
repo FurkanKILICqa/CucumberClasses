b0b133117a6d026616b0b39435800413f18d2474
Furkan Kılıc <fkrx928@gmail.com>
2023-12-12 12:40:24 +0300

    my commit

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -2,9 +2,29 @@ package runners;
 
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+   features = "src\\test\\resources\\features\\firstFeatureFile.feature",
+        glue = "stepDefinitions",
+        tags = "@US001",
+        dryRun = false
+        //dryRun da trure dersek featurefile da sarılık varmı diye kontrol eder false derssek direk çalistirir
 
+
+)
 public class Runner {
+    /*
+    Runner classin bodysi icinde birsey yazilmaz
+    Runner classinda önemli olan iki tane notasyon vardir
+    1)@RunWith ==>Runner clasimiza calisma özelligi getirdi
+    2)@CucumberOptions ==> parantezi icinde hangi testleri calistiracağımızı,
+                    hangi raporlari alacağımızı, features ve glue parametreleri ile bu dosyalarin pathlerini
+                    gibi test ayrintilarini tanimlariz ve bazi pluginler ekleyebiliriz
+      dryRun = true  ==> iken feature file daki test senaryolarini calistirmadan, eksik
+      stepDefinition i olup olmadigini kontrol eder, browseri calistirmaz
+
+ */
 }
--- src/test/resources/features/firstFeatureFile.feature
+++ src/test/resources/features/firstFeatureFile.feature
@@ -1,4 +1,6 @@
+@US001
 Feature: US001 Amazon Home Page Test(Her test feature altında kullanılır)
+
   Scenario: TC01 Arama kutusunda iphone aratılır
 
   (Given When And vb. isimleri naming convention açısından önemlidir yoksa isimlerin ne olduğunun bir önemi yoktur)
