9e1fe7d33e87d0d8a9726a7913bec20be6167984
Furkan Kılıc <fkrx928@gmail.com>
2023-12-12 12:52:51 +0300

    my commit

---
+++ src/test/java/stepDefinitions/BlueRentalStepDefinition.java
@@ -0,0 +1,9 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.When;
+
+public class BlueRentalStepDefinition {
+    @When("login butonuna tiklar")
+    public void loginButonunaTiklar() {
+    }
+}
