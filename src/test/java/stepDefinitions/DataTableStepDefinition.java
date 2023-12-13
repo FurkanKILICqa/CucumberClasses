f7a3ccca06793d22d7d9604e89900f34b043fe7c
Furkan Kılıc <fkrx928@gmail.com>
2023-12-13 22:52:11 +0300

    my commit

--- src/test/java/stepDefinitions/DataTableStepDefinition.java
+++ src/test/java/stepDefinitions/DataTableStepDefinition.java
@@ -14,7 +14,7 @@ public class DataTableStepDefinition {
     }
 
     @And("kullanici verilen bilgileri girer {string}, {string}, {string}, {string}, {string}, {string}, {string}")
-    public void kullaniciVerilenBilgileriGirer(String name, String lastName, String arg2, String arg3, String arg4, String arg5, String arg6) {
+    public void kullaniciVerilenBilgileriGirer(String name, String lastName, String position, String office, String arg4, String arg5, String arg6) {
     }
 
     @And("create butonuna basar")
