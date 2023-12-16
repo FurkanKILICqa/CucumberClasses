ad22eabef630ba7dc360a3c05270a90ed74fe3e4
Furkan Kılıc <fkrx928@gmail.com>
2023-12-16 21:10:24 +0300

    my commit

---
+++ src/test/java/runners/ParallelRunner2.java
@@ -0,0 +1,36 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        tags = "@US010",
+        dryRun = false
+        //monochrome = true pretty ile yeşil verilen detayları siyah yapar
+//dryRun da trure dersek featurefile da sarılık varmı diye kontrol eder false derssek direk çalistirir
+
+
+)
+public class ParallelRunner2 {
+    /*
+    Runner classin bodysi icinde birsey yazilmaz
+    Runner classinda önemli olan iki tane notasyon vardir
+    1)@RunWith ==>Runner clasimiza calisma özelligi getirdi
+    2)@CucumberOptions ==> parantezi icinde hangi testleri calistiracağımızı,
+                    hangi raporlari alacağımızı, features ve glue parametreleri ile bu dosyalarin pathlerini
+                    gibi test ayrintilarini tanimlariz ve bazi pluginler ekleyebiliriz
+      dryRun = true  ==> iken feature file daki test senaryolarini calistirmadan, eksik
+      stepDefinition i olup olmadigini kontrol eder, browseri calistirmaz
+
+ */
+}
