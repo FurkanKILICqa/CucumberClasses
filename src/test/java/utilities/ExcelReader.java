a13f34d1991dee7d9a39d22f4e68d827f3813af4
Furkan Kılıc <fkrx928@gmail.com>
2023-12-19 00:02:53 +0300

    my commit

--- src/test/java/utilities/ExcelReader.java
+++ src/test/java/utilities/ExcelReader.java
@@ -1,4 +1,25 @@
 package utilities;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ExcelReader {
+
+    Workbook workbook;
+    Sheet sheet;
+
+    //Bu classtan object olusturmamizin amaci excel dosyasini okumaktir
+    public ExcelReader(String dosyayolu,String sayfaIsmi){
+        try {
+            FileInputStream fis = new FileInputStream(dosyayolu);
+            workbook= WorkbookFactory.create(fis);
+            sheet=workbook.getSheet(sayfaIsmi);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
