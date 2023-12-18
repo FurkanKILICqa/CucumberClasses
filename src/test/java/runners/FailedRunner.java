eaac3b570f7a5db027b5f63347512df95ae74f27
Furkan Kılıc <fkrx928@gmail.com>
2023-12-19 01:41:23 +0300

    my commit

--- src/test/java/runners/FailedRunner.java
+++ src/test/java/runners/FailedRunner.java
@@ -1,6 +1,25 @@
 package runners;
 
-public class FailedRunner {
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",//==> console da scenariolar ile ilgili ayrintili bilgi verir
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:TestOutput/failed_scenario.txt" },
+        features = "@TestOutput/failed_scenario.txt",
+        glue = {"stepDefinitions","hooks"},
+        dryRun = false,
+        monochrome = true
+
+)
+public class FailedRunner{
 
 
 
